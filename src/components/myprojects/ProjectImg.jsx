b57bc6ad83cd3df53077a63a41b96471cf514600
Khojiakbarkhon <isakov56cc@gmail.com>
2021-05-15 10:03:57 +0200

    changes

--- src/components/myprojects/ProjectImg.jsx
+++ src/components/myprojects/ProjectImg.jsx
@@ -5,7 +5,10 @@ export default function ProjectImg() {
     return (
         <div className="m-5">
             <div className="back-img bg-secondary">
-                <div className="front-img bg-primary"></div>
+                <div className="front-img bg-primary d-flex py-3 pl-3">
+                    <div className='little-img mr-2'></div>
+                    <div className='bigger-img ml-2'></div>
+                </div>
             </div>
         </div>
     )
